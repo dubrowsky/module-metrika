fd16a5ea4a595d7fee877dc23188de7646cfb964
dubr <dubr.cola@gmail.com>
2017-12-30 10:02:11 +0300

    add jquery / floxim.js

--- Module.php
+++ Module.php
@@ -6,6 +6,10 @@ use \Floxim\Floxim\System\Fx as fx;
 class Module extends \Floxim\Floxim\Component\Module\Entity {
     public function init() {
         $dir = fx::path()->abs('/module/Dubr/Metrika');
-        fx::page()->addJs([$dir.'/metrika.js']);
+        fx::page()->addJs([
+            FX_JQUERY_PATH_HTTP,
+            fx::path()->abs('/module/Floxim/Ui/Js/Floxim.js'),
+            $dir.'/metrika.js'
+        ]);
     }
 }
